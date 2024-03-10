fe72215d1736f1c7924abdde882db0217cc14271
Mert Yılmaz <mrtylz1509@gmail.com>
2024-03-10 18:06:05 +0300

    örnek

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const[paragraftGoster, paragraftIslem]=useState(true)
+  const[sayac, sayacGuncelle] = useState(0)
 
-  useEffect( function() {})
+  useEffect( ()=> {console.log("1. efect çalıştı");},[] )
 
 
+   useEffect(()=>{console.log("2. efect calıştı");})
+ 
   return (
     <>
-      <p>App</p>
+     sayac:{sayac}
+     
     </>
   );
 }
