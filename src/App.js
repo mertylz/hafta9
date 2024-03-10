6e5bbeceef6f397c17deff1b0bde4f4a5357253d
Mert Yılmaz <mrtylz1509@gmail.com>
2024-03-10 18:05:22 +0300

    örnek kodlar yazıdı

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect } from 'react';
+
+function App() {
+
+  useEffect( function() {})
+
+
+  return (
+    <>
+      <p>App</p>
+    </>
+  );
+}
+
+export default App;
